b864ebd6f1da4407676c501d155fef0850897979
OnyxFlames <jacobhicks1996@hotmail.com>
2017-12-31 18:43:08 -0500

    Fixed divisor in functions to be more accurate

--- lsdir/Utils.hpp
+++ lsdir/Utils.hpp
@@ -4,9 +4,18 @@
 #include <string>
 #include <vector>
 
+enum magnitude : uintmax_t
+{
+	byte = 1000,
+	kilobyte = byte * 1000,
+	megabyte = kilobyte * 1000,
+	gigabyte = megabyte * 1000,
+	terabyte = gigabyte * 1000
+};
+
 const std::vector<std::string> list_drives();
 
-const std::string to_shortsuffix(uintmax_t num, unsigned short divisor = 1024);
-const std::string to_longsuffix(uintmax_t num, unsigned short divisor = 1024);
+const std::string to_shortsuffix(uintmax_t num, unsigned short divisor = byte);
+const std::string to_longsuffix(uintmax_t num, unsigned short divisor = byte);
 
-const double to_smallestmagnitude(uintmax_t num, unsigned short divisor = 1024);
+const double to_smallestmagnitude(uintmax_t num, unsigned short divisor = byte);
